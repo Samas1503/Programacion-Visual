97d203e16c2c74fac12a72a5aadeb429d2842f4e
Samas1503 <https://github.com/Samas1503>
2021-04-07 16:56:05 -0300

    correccion de archivos

--- Practica/TPN1/ejercicio_3/src/main/java/ar/edu/unju/edm/models/mostrarFormatoSimple.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unju.edm.models;
-
-public class mostrarFormatoSimple {
-    public String simple(procesoFecha fecha){
-        return (fecha.getDia() + "-" + fecha.getMes() + "-" + fecha.getAnio());
-    }
-}
---
+++ Practica/tp_1/src/test/java/ar/edu/unju/edm/tp_1/Tp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.tp_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
